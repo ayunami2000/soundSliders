38c1dac4215abc7547c7827db1b4a274f5acd12a
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-20 11:08:39 -0500

    Create sound.js

---
+++ sound.js
@@ -0,0 +1,7 @@
+try{
+req=new XMLHttpRequest();
+req.open('GET',window.soundURL,true);
+req.responseType='arraybuffer';
+req.onload=function(){window.oof=req.response;for(var i=0;i<document.querySelectorAll('input').length;i++){document.querySelectorAll('input')[i].removeAttribute('disabled');}}
+req.send();
+}catch(e){alert(e);}
