e0bceda39f3bff35eecd9188638b7ef230c81c31
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-20 17:08:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,15 @@ document.body.innerHTML+="<img id='btn' src='"+window.location.hash.slice(1)+".p
 document.getElementById('btn').ontouchend=function(){window.playSound()};
 document.getElementById('btn').onmouseup=function(){(!('ontouchend' in window))&&(window.playSound())};
 }else{document.body.innerHTML="Error: Invalid sound. <a href='index.html'>Click here to see the sound directory...</a>";}
+window.audioCtx=0;
 var source;
 function getData(){try{
-if(!audioCtx){audioCtx=new (window.AudioContext||window.webkitAudioContext)();}
-source=audioCtx.createBufferSource();
-audioCtx.decodeAudioData(window.sound.slice(0),function(buffer){
+if(!window.audioCtx){window.audioCtx=new (window.AudioContext||window.webkitAudioContext)();}
+source=window.audioCtx.createBufferSource();
+window.audioCtx.decodeAudioData(window.sound.slice(0),function(buffer){
 source.buffer=buffer;
 source.playbackRate.value=document.querySelector('.playback-rate-control').value;
-source.connect(audioCtx.destination);
+source.connect(window.audioCtx.destination);
 },function(e){console.log(e);});
 }catch(e){alert(e);}}
 window.playSound=function(){
@@ -29,4 +30,4 @@ source.playbackRate.value=document.querySelector('.playback-rate-control').value
 document.querySelector('.playback-rate-control').oninput=function(){document.querySelector('.playback-rate-value').innerHTML=document.querySelector('.playback-rate-control').value;(document.querySelector('input[type=checkbox]').checked)&&window.playSound();}
 }
 window.onhashchange=function(){window.location.reload();}
-window.onclick=function(){if(!audioCtx){audioCtx=new (window.AudioContext||window.webkitAudioContext)();}this.onclick=function(e){};}
+window.onclick=function(){if(!window.audioCtx){window.audioCtx=new (window.AudioContext||window.webkitAudioContext)();}this.onclick=function(e){};}
