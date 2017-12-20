56cbed11c898cf4c904e5d1bd88cbee573f10e01
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-20 17:48:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ source.connect(window.audioCtx.destination);
 source.start(0);
 },function(e){alert(e);});
 }catch(exc){alert(exc);}}
-document.querySelector('.playback-rate-control').oninput=function(){document.querySelector('.playback-rate-value').innerHTML=document.querySelector('.playback-rate-control').value/10;(document.querySelector('input[type=checkbox]').checked)&&window.getData();}
+document.querySelector('.playback-rate-control').oninput=function(){document.querySelector('.playback-rate-value').innerHTML=document.querySelector('.playback-rate-control').value;(document.querySelector('input[type=checkbox]').checked)&&window.getData();}
 }
 window.onhashchange=function(){window.location.reload();}
