5fbc209a5dd5b536038a387a99e33cdc38462bca
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-21 07:53:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 try{
 req=new XMLHttpRequest();
-req.open('GET',window.soundURL,true);
+req.open('GET',window.location.hash.slice(1),true);
 req.responseType='arraybuffer';
 req.onload=function(){window.sound=req.response;for(var i=0;i<document.querySelectorAll('input').length;i++){document.querySelectorAll('input')[i].removeAttribute('disabled');}}
 req.send();
