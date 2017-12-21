52ce2adaf6d00f31cd3eb895552df8226484d4b8
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-21 15:07:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,34 @@ req.onload=function(){window.sound=req.response;for(var i=0;i<document.querySele
 req.send();
 }catch(e){alert(e);}
 window.onload=function(){
-document.body.innerHTML+="<img id='btn' src='sounds/"+window.location.hash.slice(1).replace(/\W/g,'')+"/the.png' style='cursor:pointer;width:10em;height:10em;'/>";
-document.getElementById('btn').ontouchstart=function(){window.getData()};
-document.getElementById('btn').onmousedown=function(){(!('ontouchstart' in window))&&(window.getData())};
-document.getElementById('btn').onerror=function(){document.body.innerHTML="Error: Invalid sound.<br><a href='index.html'>Click to go to the directory...</a>";}
-window.audioCtx=0;
+document.body.innerHTML+="<img src='sounds/"+window.location.hash.slice(1).replace(/\W/g,'')+"/the.png' style='cursor:pointer;width:10em;height:10em;'/>";
+document.querySelector('img').ontouchstart=function(){window.getData()};
+document.querySelector('img').onmousedown=function(){(!('ontouchstart' in window))&&(window.getData())};
+document.querySelector('img').onerror=function(){document.body.innerHTML="Error: Invalid sound.<br><a href='index.html'>Click to go to the directory...</a>";}
+window.audioCtx;
+window.objURL;
+document.querySelector('input[type=file]').onchange=function(){
+for(var i=0;i<document.querySelectorAll('input').length;i++){document.querySelectorAll('input')[i].disabled=true;}
+req=new XMLHttpRequest();
+window.objURL=window.URL.createObjectURL(this.files[0]);
+req.open('GET',window.objURL,true);
+req.responseType='arraybuffer';
+req.onload=function(){window.URL.revokeObjectURL(window.objURL);window.sound=req.response;for(var i=0;i<document.querySelectorAll('input').length;i++){document.querySelectorAll('input')[i].removeAttribute('disabled');}}
+req.send();
+}
 var source;
 window.getData=function(){try{
 if(!window.audioCtx){window.audioCtx=new (window.AudioContext||window.webkitAudioContext)();}
 source=window.audioCtx.createBufferSource();
 window.audioCtx.decodeAudioData(window.sound.slice(0),function(buffer){
 source.buffer=buffer;
-source.playbackRate.value=document.querySelector('.playback-rate-control').value/100;
+source.playbackRate.value=document.querySelector('input[type=range]').value/100;
 source.connect(window.audioCtx.destination);
 source.start(0);
 },function(e){alert(e);});
 }catch(exc){alert(exc);}}
-document.querySelector('.playback-rate-control').oninput=function(){document.querySelector('.playback-rate-value').value=document.querySelector('.playback-rate-control').value;(document.querySelector('input[type=checkbox]').checked)&&window.getData();}
-document.querySelector('.playback-rate-control').onchange=function(){document.querySelector('.playback-rate-value').value=document.querySelector('.playback-rate-control').value;(!document.querySelector('input[type=checkbox]').checked)&&window.getData();}
-document.querySelector('.playback-rate-value').onchange=function(){document.querySelector('.playback-rate-control').value=document.querySelector('.playback-rate-value').value;window.getData();}
+document.querySelector('input[type=range]').oninput=function(){document.querySelector('input[type=number]').value=this.value;(document.querySelector('input[type=checkbox]').checked)&&window.getData();}
+document.querySelector('input[type=range]').onchange=function(){document.querySelector('input[type=number]').value=this.value;(!document.querySelector('input[type=checkbox]').checked)&&window.getData();}
+document.querySelector('input[type=number]').onchange=function(){document.querySelector('input[type=range]').value=this.value;window.getData();}
 }
 window.onhashchange=function(){window.location.reload();}
