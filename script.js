c5b48d6768c6d9f529a949585a5f3fef1216c3cd
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-21 09:50:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ req.send();
 }catch(e){alert(e);}
 window.onload=function(){
 if(window.location.hash=="#oof"||window.location.hash=="#woah"||window.location.hash=="#bork"||window.location.hash=="#yee"||window.location.hash=="#toad"){
-document.body.innerHTML+="<img id='btn' src='sounds/"+window.location.hash.slice(1).replace(/\W/g,'')+"/"+window.location.hash.slice(1).replace(/\W/g,'')+".mp3' style='cursor:pointer;width:10em;height:10em;'/>";
+document.body.innerHTML+="<img id='btn' src='sounds/"+window.location.hash.slice(1).replace(/\W/g,'')+"/"+window.location.hash.slice(1).replace(/\W/g,'')+".png' style='cursor:pointer;width:10em;height:10em;'/>";
 document.getElementById('btn').ontouchstart=function(){window.getData()};
 document.getElementById('btn').onmousedown=function(){(!('ontouchstart' in window))&&(window.getData())};
 }else{document.body.innerHTML="Error: Invalid sound. <a href='index.html'>Click here to see the sound directory...</a>";}
