d78c04bb22978271470dac1310a5c5e66be8cd7a
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-22 16:06:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -81,10 +81,11 @@ return;
 }
 window.choosingPreset=1;
 for(var i=0;i<document.querySelectorAll('input:not([name=preset])').length;i++){document.querySelectorAll('input:not([name=preset])')[i].disabled=true;}
-for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.removeAttribute('disabled');document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.readonly=true;}
+for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){!document.querySelectorAll('input[type=checkbox]')[1].checked&&document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.removeAttribute('disabled');document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.readonly=true;}
 this.innerHTML="Cancel...";
 }
 for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.oninput=function(){if(window.choosingPreset){return;}this.previousElementSibling.previousElementSibling.value=this.value;for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].checked=document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.value==document.querySelector('input[type=range]').value;}}}
+document.querySelectorAll('input[type=checkbox]').onchange=function(){for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]').nextElementSibling.nextElementSibling.disabled=document.querySelectorAll('input[type=checkbox]')[1].checked;}}
 }//end show directory else
 }
 window.onhashchange=function(){window.location.reload();}
