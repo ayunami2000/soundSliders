f5ad08e31105762f56b48eb997accfd5c0ea4ec3
ayunami2000 <spwilliamsiam@gmail.com>
2017-12-22 13:56:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -42,8 +42,15 @@ document.querySelector('input[type=range]').onchange=function(){for(var i=0;i<do
 document.querySelector('input[type=number]').onchange=function(){for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].checked=document.querySelectorAll('input[name=preset]')[i].value==this.value;}document.querySelector('input[type=range]').value=this.value;window.getData();}
 for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){
 document.querySelectorAll('input[name=preset]')[i].onchange=function(){if(this.checked){document.querySelector('input[type=range]').value=document.querySelector('input[type=number]').value=this.value;}}
-document.querySelectorAll('input[name=preset]')[i].onclick=function(){!window.choosingPreset&&window.getData();}
-document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.onclick=function(){if(window.choosingPreset){return;}this.previousElementSibling.previousElementSibling.click();}
+document.querySelectorAll('input[name=preset]')[i].onclick=function(){
+if(window.choosingPreset){
+this.nextElementSibling.nextElementSibling.value=this.value=document.querySelector('input[type=range]').value;
+for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].checked=i==this.nextElementSibling.value-1;}
+return;
+}
+window.getData();
+}
+document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.onclick=function(){this.previousElementSibling.previousElementSibling.click();}
 }
 document.querySelectorAll('button')[1].onclick=function(){
 if(window.choosingPreset){
@@ -53,8 +60,8 @@ return;
 }
 window.choosingPreset=1;
 for(var i=0;i<document.querySelectorAll('input').length;i++){document.querySelectorAll('input')[i].disabled=true;}
-document.querySelectorAll('input[name=preset]')[this.nextElementSibling.value-1].nextElementSibling.nextElementSibling.value=document.querySelectorAll('input[name=preset]')[this.nextElementSibling.value-1].value=document.querySelector('input[type=range]').value;
-for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].checked=i==this.nextElementSibling.value-1;}
+//document.querySelectorAll('input[name=preset]')[this.nextElementSibling.value-1].nextElementSibling.nextElementSibling.value=document.querySelectorAll('input[name=preset]')[this.nextElementSibling.value-1].value=document.querySelector('input[type=range]').value;
+//for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].checked=i==this.nextElementSibling.value-1;}
 }
 for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[type=number]')[i].oninput=function(){this.previousElementSibling.previousElementSibling.value=this.value;for(var i=0;i<document.querySelectorAll('input[name=preset]').length;i++){document.querySelectorAll('input[name=preset]')[i].checked=document.querySelectorAll('input[name=preset]')[i].nextElementSibling.nextElementSibling.value==document.querySelector('input[type=range]').value;}}}
 }//end show directory else
